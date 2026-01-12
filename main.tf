295ce0616cb71412f9c77af232e997aa3be4f0ab
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-12 16:57:57 +0530

    Create main.tf
    Creation of an AWS EC2 instance.

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}

03f7ccf4eefbc75f38e55181c978cbc0ab33359f
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-12 17:18:54 +0530

    Update variables.tf
    remove ami variable

--- variables.tf
+++ variables.tf
@@ -3,10 +3,6 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   default     = "t3.micro"
